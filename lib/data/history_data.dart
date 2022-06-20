e5d92e78f48848fffdc51af8e7bc6eb07086114e
Alex <amoahtnt6@gmail.com>
2022-06-20 23:38:44 +0000

    History Page done

---
+++ lib/data/history_data.dart
@@ -0,0 +1,77 @@
+const historyData = [
+  {
+    "date": "Thu, 12 May",
+    "data": [
+      {
+        "task": "Laundry",
+        "content": "Your laundry will be picked up today. We can't wait",
+        "time": "10:40am - 12:30pm"
+      },
+      {
+        "task": "Moving",
+        "content": "You sent Naa Mansa to Dafana",
+        "time": "10:40am - 12:30pm"
+      },
+      {
+        "task": "Laundry",
+        "content": "Your laundry will be picked up today. We can't wait",
+        "time": "12:00pm - 2:30pm"
+      },
+      {
+        "task": "Cleaning",
+        "content": "Your Cleaning will be done up today. We can't wait",
+        "time": "1:00pm - 4:30pm"
+      },
+    ],
+  },
+  {
+    "date": "Thu, 14 May",
+    "data": [
+      {
+        "task": "Cleaning",
+        "content": "Your Cleaning will be done up today. We can't wait",
+        "time": "10:40am - 12:30pm"
+      },
+      {
+        "task": "Laundry",
+        "content": "Your laundry will be picked up today. We can't wait",
+        "time": "12:00pm - 2:30pm"
+      },
+       {
+        "task": "Moving",
+        "content": "You sent Naa Mansa to Dafana",
+        "time": "10:40am - 12:30pm"
+      },
+      {
+        "task": "Cleaning",
+        "content": "Your Cleaning will be done up today. We can't wait",
+        "time": "1:00pm - 4:30pm"
+      },
+    ],
+  },
+  {
+    "date": "Thu, 6 Aug",
+    "data": [
+      {
+        "task": "Laundry",
+        "content": "Your laundry will be picked up today. We can't wait",
+        "time": "10:40am - 12:30pm"
+      },
+       {
+        "task": "Moving",
+        "content": "You sent Naa Mansa to Dafana",
+        "time": "10:40am - 12:30pm"
+      },
+      {
+        "task": "Laundry",
+        "content": "Your laundry will be picked up today. We can't wait",
+        "time": "12:00pm - 2:30pm"
+      },
+      {
+        "task": "Cleaning",
+        "content": "Your Cleaning will be done up today. We can't wait",
+        "time": "1:00pm - 4:30pm"
+      },
+    ],
+  }
+];
--- lib/screens/dashboard/history/history_screen.dart
+++ lib/screens/dashboard/history/history_screen.dart
@@ -1,14 +1,160 @@
 import 'package:flutter/material.dart';
-import 'package:viva_service_provider/shared/custom_colors.dart';
 
+import '../../../data/history_data.dart';
+import '../../../shared/custom_colors.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size _size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: movingColor,
+      backgroundColor: whiteColor,
+      appBar: AppBar(
+        elevation: .0,
+        backgroundColor: whiteColor,
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'History',
+          style: TextStyle(
+            color: blackColor,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: _size.height * 0.1,
+              child: const Center(
+                child: Divider(thickness: 1.5),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: historyData.map((e) {
+                    List data = e['data'] as List;
+                    return Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            e['date'].toString(),
+                            style: const TextStyle(
+                              color: blackColor,
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          ListView.builder(
+                            itemCount: data.length,
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  width: double.infinity,
+                                  padding:
+                                      const EdgeInsets.fromLTRB(10, 5, 10, 15),
+                                  margin: const EdgeInsets.symmetric(
+                                      vertical: 5, horizontal: 5),
+                                  height: _size.height * 0.2,
+                                  decoration: BoxDecoration(
+                                    color: data[index]['task'] == 'Moving'
+                                        ? movingLightColor
+                                        : data[index]['task'] == 'Cleaning'
+                                            ? cleaningLightColor
+                                            : laundryLightColor,
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            margin: const EdgeInsets.symmetric(
+                                                vertical: 10),
+                                            child: Text(
+                                              data[index]['task'],
+                                              style: const TextStyle(
+                                                color: primaryColor,
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 15,
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(width: 10),
+                                          Image.asset(
+                                            data[index]['task'] == 'Moving'
+                                                ? 'assets/images/truck.png'
+                                                : data[index]['task'] ==
+                                                        'Cleaning'
+                                                    ? 'assets/images/broom.png'
+                                                    : 'assets/images/shirt.png',
+                                            width: _size.width * .06,
+                                          ),
+                                        ],
+                                      ),
+                                      Text(
+                                        data[index]['content'],
+                                        style: const TextStyle(
+                                          color: greyColor,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: double.infinity,
+                                        height: _size.height * 0.04,
+                                        child: const Center(
+                                          child: Divider(
+                                            thickness: 1.5,
+                                            color: primaryLightColor,
+                                          ),
+                                        ),
+                                      ),
+                                      Row(
+                                        children: const [
+                                          Icon(Icons.access_time,
+                                              color: primaryColor),
+                                          SizedBox(width: 10),
+                                          Text(
+                                            '10:30am - 12:00pm',
+                                            style: TextStyle(
+                                              color: greyColor,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 15,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ]);
+                  }).toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
